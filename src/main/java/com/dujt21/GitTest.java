727e1e89b28f500c766e9ab4fea6fb0432295493
MingTian <MingTian@xaigc.com>
2022-08-10 21:15:42 +0800

    push commit

--- src/main/java/com/dujt21/GitTest.java
+++ src/main/java/com/dujt21/GitTest.java
@@ -7,5 +7,8 @@ public class GitTest {
         System.out.println("hello, git4");
         System.out.println("master");
         System.out.println("hot-fix");
+
+
+        System.out.println("push test");
     }
 }
